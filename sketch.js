42332e3658fd479afb015675f591a08e9fd7cdb6
Troy Petty <58780638+troydatboi134@users.noreply.github.com>
2020-02-12 08:28:34 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -40,7 +40,7 @@ group.add(sprite);
     if (coincollect>50){coincollect=50}
 
   if (sprite.x>900){
-  window.location.href = "https://bigboybryson.github.io/theplanegame/";
+  window.location.href = "troydatboi134.github.io/coin-runner/";
 }
   
     background("blue");
