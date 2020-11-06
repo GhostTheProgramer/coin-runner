63c4e82d0c340879b3d7c3cb8aea0e07de8525ba
Troy Petty <58780638+troydatboi134@users.noreply.github.com>
2020-11-06 08:24:34 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,19 +31,7 @@ group.add(sprite);
 
 
   
- function draw() {
-     if (keyDown("right")) {
-       timebak=time;
-         planexbak=sprite.x;
-  planeybak=sprite.y;
-  coinxbak=sprite2.x;
-  coinybak=sprite2.y;
-  scorebak=score;
-   coincollectbak=coincollect;
-    }
-   
 
-  
 
       
   time=time+1
