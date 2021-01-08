628631a685216a0ea362983fb2965f40043e32cc
Troy Petty <58780638+troydatboi134@users.noreply.github.com>
2021-01-08 08:24:05 -0600

    Update sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -1,26 +1,2 @@
-var web = prompt("What's version of the plane game do you want to play,bot or normal");
-switch(web) {
-  case "Normal":
+
    window.location.href = "/coin-runner/index2.html";
-break;
-
-  case "Bot":
-     window.location.href = "/coin-runner/index3.html"
-    break;
-
-      default:
- alert("bot or normal")
-    var web = prompt("What's version of the plane game do you want to play,bot or normal");
-switch(web) {
-  case "Normal":
-   window.location.href = "/coin-runner/index2.html";
-break;
-
-  case "Bot":
-     window.location.href = "/coin-runner/index3.html"
-    break;
-}
-
-   
-}
-
