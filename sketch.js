548821d369caade433c2eb38ee6f0c943f3e8e29
Troy Petty <58780638+troydatboi134@users.noreply.github.com>
2021-01-12 08:42:58 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,7 @@
-function preload(){
-  mySound = loadSound('ppe.mp3');
 
-}
 function setup(){  
     createCanvas(860,400);
-
-time = 0;
-
-  sprite2=createSprite(700,200,0,0);
+sprite2=createSprite(700,200,0,0);
 img=loadImage("e.png")
 sprite2.addImage(img);
 sprite2.scale=0.5
@@ -17,70 +11,36 @@ sprite.addImage(img);
   group = createGroup();
 group.add(sprite);
   score = 0 ;
-  scorebak=0;
-  pause = 0;
-  pause2 = .5; 
-  coincollect = 7;
-  coincollectbak=0;
-  time2 = 0;
-  timebak=0;
-  
+ coincollect = 7;
 }
-
-
-
-
-  
- function draw() {
-  
-
-      
-  time=time+1
-  
-  
-  textSize(50);
+function draw() {
+ time=time+1
+ textSize(50);
   sprite.scale=.50;
     if (coincollect>45){coincollect=4}
-
   if (sprite.x>860){
   window.location.href = "https://troydatboi134.github.io/coin-runner/";
 }
-  
-    background("blue");
-  
-  
-  if (sprite.x===sprite.x) {
+ background("blue");
+ if (sprite.x===sprite.x) {
     sprite.y = mouseY;
           sprite.x = sprite.x+coincollect;
-    
-
-    
-
-   
-    
    if (group.isTouching(sprite2)) {
        mySound.play();
      sprite.x=0;
    coincollect=coincollect+1;
              score = score+1;
-
-      sprite2.y=random(50,365); 
+     sprite2.y=random(50,365); 
       }
-
-
-    fill("yellow");
-                
- text('speed',192,40 );
+   fill("yellow");
+   text('speed',192,40 );
 fill("red");
-                  
-text(coincollect,192,82 );
+ text(coincollect,192,82 );
     fill("yellow");
 fill("red");
-
-  text("score",50,300 );
+   text("score",50,300 );
    fill("red");
-    
-    text(score,100,342 );
+   text(score,100,342 );
     fill("yellow");
     drawSprites();
  }
